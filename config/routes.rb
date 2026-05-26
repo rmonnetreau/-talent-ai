cdf345824ddac632b9ab3143fddc607752b88846
Yoann Kermet <y.kermet@gmail.com>
2026-05-26 11:12:40 +0200

    add first routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   get "up" => "rails/health#show", as: :rails_health_check
+
+  resources :interviews, only: [:index, :show] do
+    resources :chats, only: [:create]
+  end
+
+  resources :chats, only: :show do
+    resources :messages, only: [:create]
+  end
 end
