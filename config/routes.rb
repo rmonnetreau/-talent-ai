8d4cfdc448354affba2331db4593c3b8e3647b33
romain monnetreau <rmonnetreau@gmail.com>
2026-05-27 17:15:21 +0200

    feat: add profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+
   get "up" => "rails/health#show", as: :rails_health_check
 
   root to: "pages#home"
@@ -7,6 +8,8 @@ Rails.application.routes.draw do
   resources :interviews, only: [:index, :show, :create, :new, :edit, :update, :destroy] do
     resources :chats, only: [:create, :new]
   end
+
+  resource :profile, only: [:show, :new, :edit, :create, :update]
 
   resources :chats, only: :show do
     resources :messages, only: [:create]
