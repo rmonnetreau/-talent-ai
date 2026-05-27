2eba0cd9c55b6e8467b533c162dda460da250db3
Alexandre Theol <alexandre.theol@gmail.com>
2026-05-27 16:58:18 +0200

    micro

--- config/initializers/ruby_llm.rb
+++ config/initializers/ruby_llm.rb
@@ -1,8 +1,8 @@
 RubyLLM.configure do |config|
   # config.openai_api_key = ENV["OPENAI_API_KEY"]
   # config.openai_api_key = ENV["GEMINI_API_KEY"]
-  # config.openai_api_key = ENV["GITHUB_TOKEN"]
-  # config.openai_api_base = "https://models.inference.ai.azure.com"
-  config.gemini_api_key = ENV["GEMINI_API_KEY"]
-  config.gemini_api_base = "https://generativelanguage.googleapis.com/v1"
+  config.openai_api_key = ENV["GITHUB_TOKEN"]
+  config.openai_api_base = "https://models.inference.ai.azure.com"
+  # config.gemini_api_key = ENV["GEMINI_API_KEY"]
+  # config.gemini_api_base = "https://generativelanguage.googleapis.com/v1"
 end
